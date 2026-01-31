3a5e39a16ae964e6457034ec5479462f8b3fa57d
rounakkraaj-1744 <rounakkraaj707@gmail.com>
2026-02-01 00:34:25 +0530

    redis connection established using ioredis

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import express, {type Request, type Response} from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 8080;
+
+app.use (cors)
+
+app.get ("/", (req: Request, res: Response)=>{
+    res.send("Backend is running ...")
+})
+
+app.listen (port, ()=>{
+    console.log (`Server is running at http://localhost:${port}`)
+})
---
+++ redis/client.ts
@@ -0,0 +1,14 @@
+import Redis from "ioredis"
+
+export const redis = new Redis ({
+    host: process.env.REDIS_HOST || "127.0.0.1",
+    port: Number(process.env.REDIS_PORT) || 6379
+})
+
+redis.on("connect", ()=>{
+    console.log ("Redis is connected")
+})
+
+redis.on("error", (err)=>{
+    console.log ("Redis error", err)
+})
